d785181964f1cd36d169e38dcfe10e1968b03b6f
bonface221 <mainaboniface221@gmail.com>
2023-08-22 21:00:54 +0300

    fix bug

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -11,19 +11,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
 from pathlib import Path
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-load_dotenv(os.path.join(BASE_DIR, "../.env"))
-
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='7k9$k8u)vral9#uml&v&9$0nj+=g%pq#(0tyqgv4&53xha@b5d'
+SECRET_KEY = '7k9$k8u)vral9#uml&v&9$0nj+=g%pq#(0tyqgv4&53xha@b5d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
